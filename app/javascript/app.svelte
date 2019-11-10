ed5a946851487883e894ca774f8aa3d9781765ae
Yulia Oletskaya <yulia.oletskaya@gmail.com>
2019-11-10 12:53:27 +0300

    Working router

--- app/javascript/app.svelte
+++ app/javascript/app.svelte
@@ -1,6 +1,18 @@
 <script>
-  import Router from 'svelte-spa-router'
-  import { routes } from './svelte_app/routes'
+  import { Router, Link, Route } from "svelte-routing";
+  import DaysIndex from "./svelte_app/views/days/index";
+  import ProductsIndex from "./svelte_app/views/products/index";
+
+  export let url = "";
 </script>
 
-<Router {routes} />
+<Router url="{url}">
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="products">Products</Link>
+  </nav>
+  <div>
+    <Route path="/" component="{DaysIndex}" />
+    <Route path="products" component="{ProductsIndex}" />
+  </div>
+</Router>
--- app/javascript/svelte_app/views/shared/layout.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-</script>
-
-<div class="app">
-</div>
